f038204ad7906e61572635e713dd1abf9834a6c9
Saran-PANKAEW <42270435+Saran-PANKAEW@users.noreply.github.com>
2023-09-20 16:10:01 +0200

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,24 @@
+"""
+{Test script for scPROFILE v05}
+"""
+__author__ = 'Saran PANKAEW'
+__version__ = '0.5'
+__maintainer__ = 'Saran PANKAEW'
+__email__ = 'saran.pankeaw@curie.fr'
+__status__ = 'development'
+__date__ = '07/08/2023'
+
+###########
+##TO DO ###
+
+# Check how to process mutation profile
+
+###########
+
+from .exp_categorization import *
+from .exp_normalization import *
+from .mutation_processing import *
+from .models_personalization import *
+#from .models_simulation import *
+
+
